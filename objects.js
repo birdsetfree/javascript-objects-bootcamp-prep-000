422308d58767fddaa7669e2d022f49b4f195482a
Banu Akkuş <akkusbanu92@gmail.com>
2018-10-16 10:29:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { sia : "chandelier"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
+  playlist[artistName] = songTitle
   return playlist
 }
